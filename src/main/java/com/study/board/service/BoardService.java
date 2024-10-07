dab07a58f9ee9eb94528128c249fb97886120cc1
bam <juhyun19981129@gmail.com>
2024-10-07 20:46:35 +0900

    게시글 리스트 출력 구현

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -2,6 +2,7 @@ package com.study.board.service;
 
 import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +13,9 @@ public class BoardService {
 
     public void write(Board board) {
         boardRepository.save(board);
+    }
+
+    public List<Board> boardList() {
+        return boardRepository.findAll();
     }
 }
