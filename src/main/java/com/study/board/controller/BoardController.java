2f44c4e9f2bcebf5989dbc185073ca61904663e4
bam <juhyun19981129@gmail.com>
2024-10-08 17:06:29 +0900

    글 삭제, 수정, 메세지 출력 구현

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BoardController {
+
     @Autowired
     private final BoardService boardService;
 
@@ -24,10 +26,13 @@ public class BoardController {
     }
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(Board board) {
+    public String boardWritePro(Board board, Model model) {
         boardService.write(board);
 
-        return "";
+        model.addAttribute("message", "글 작성이 완료되었습니다.");
+        model.addAttribute("searchUrl", "/board/list");
+
+        return "message";
     }
 
     @GetMapping("/board/list")
@@ -41,5 +46,41 @@ public class BoardController {
         model.addAttribute("board", boardService.boardView(id));
 
         return "boardview";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(@RequestParam(name = "id") Integer id) {
+        boardService.boardDelete(id);
+
+        return "redirect:/board/list";
+    }
+
+    //@PathVariable: ()안의 내용이 인식이 되어 파라미터로 사용
+    //기존의 view?id=0 대신 view/0와 같은 식으로 표시가 된다.
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("board", boardService.boardView(id));
+
+        return "boardmodify";
+    }
+
+    /*
+    *   실무에서는 이렇게 덮어 씌우는 방식의 수정은 금지(JPA 사용 시). JPA의 Dirty Checking을 사용하자
+    *   JPA의 Dirty Checking 기능은 인해 트랜젝션이 종료되는 시점에 자동으로 변경 사항을 DB에 반영한다.
+     */
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, Model model) {
+        //기존 글이 담기는 임시 객체
+        Board boardTemp = boardService.boardView(id);
+
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+
+        boardService.write(boardTemp);
+
+        model.addAttribute("message", "글 수정이 완료되었습니다.");
+        model.addAttribute("searchUrl", "/board/list");
+
+        return "message";
     }
 }
